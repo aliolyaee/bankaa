862824f9a93e72432e8592604584b390b3d95872
aliolyaee3@gmail.com <aliolyaee3@gmail.com>
2025-08-26 05:38:23 +0330

    add anjoman font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type {Config} from 'tailwindcss';
+import type { Config } from 'tailwindcss';
 
 export default {
   darkMode: ['class'],
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ["Anjoman", 'Inter', 'sans-serif'],
+        headline: ["Anjoman", 'Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
